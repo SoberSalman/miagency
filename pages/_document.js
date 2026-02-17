4ddfab6338686e0a0f1a648efd10da12bcc94a53
salman-taleemabad <salman.ahmad@taleemabad.com>
2026-02-17 23:11:39 +0500

    Remove Google Analytics environment variable - will add later when needed
    Co-Authored-By: SoberSalman

--- pages/_document.js
+++ pages/_document.js
@@ -24,21 +24,6 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-
-        {/* Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-XXXXXXXXXX', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
       </Head>
       <body className="bg-white">
         <Main />
